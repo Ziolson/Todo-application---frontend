e6903126d98353903171eacf31a1d4d31ac95187
Mariusz Ziółek <ziolson92@gmail.com>
2020-10-19 16:05:08 +0200

    Implemented Counter example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import FirstComponent from './components/learning-examples/FirstComponent'
 import SecondComponent from './components/learning-examples/SecondComponent'
 import ThirdComponent from './components/learning-examples/ThirdComponent'
+import Counter from './components/counter/Counter'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter/>
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
         My Hello World
         <FirstComponent />
         <SecondComponent></SecondComponent>
